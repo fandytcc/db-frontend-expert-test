bf7d46f1d3465766abd32b5e2e85d816837d35c0
fandytcc <fandytsui@gmail.com>
2018-03-15 18:38:27 +0100

    define components, set up createMuiTheme in app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import Navigation from './components/Navigation'
+import Map from './components/Map'
+import Places from './components/Places'
+//styling
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import createMuiTheme from './styles/theme'
+import './App.css'
 
 class App extends Component {
+
+  static childContextTypes = {
+    createMuiTheme: PropTypes.object.isRequired,
+  }
+
+  getChildContext() {
+    return { createMuiTheme }
+  }
+
+
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <MuiThemeProvider theme={createMuiTheme}>
+        <div className="App">
+          <header className="App-header">
+          </header>
+          <p className="App-intro">
+          </p>
+        </div>
+      </MuiThemeProvider>
+    )
   }
 }
 
-export default App;
+export default App
